c35e3fd6efde479889da6813ba8ca88b54a90020
ShizukanaOtaku <otakushizukana@gmail.com>
2025-04-08 15:43:07 +0200

    handle initialization failure gracefully

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include "rendering/window.hpp"
 #include <cstdio>
+#include <cstdlib>
 #include <event/key_event.hpp>
+#include <iostream>
 #include <tiny_cherno.hpp>
 
 int main() {
     tiny_cherno::WindowParameters params =
         tiny_cherno::WindowParameters{"Test TinyCherno App", 800, 600, false};
-    tiny_cherno::init(params);
+    tiny_cherno::InitializationError error = tiny_cherno::init(params);
+    if (error != tiny_cherno::InitializationError::NONE) {
+        std::cerr << "Initializing the TinyCherno runtime failed with code " << error << '\n';
+        exit(EXIT_FAILURE);
+    }
+
     tiny_cherno::TinyChernoRuntime::GetRuntime()
         ->event_dispatcher.RegisterListener(
             tiny_cherno::EventType::KeyEvent, [](tiny_cherno::Event &e) {
