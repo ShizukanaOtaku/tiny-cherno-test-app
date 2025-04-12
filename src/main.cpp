d34a6a1855a42e983d02b06911d7d54d845af59f
ShizukanaOtaku <otakushizukana@gmail.com>
2025-04-12 13:34:12 +0200

    Test a scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "rendering/window.hpp"
+#include "entity/entity.hpp"
+#include "scene/scene.hpp"
 #include <cstdio>
 #include <cstdlib>
 #include <event/key_event.hpp>
 #include <iostream>
+#include <memory>
 #include <tiny_cherno.hpp>
 
 int main() {
@@ -14,15 +17,17 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    tiny_cherno::Scene scene;
+    scene.Entities.push_back(std::make_shared<tiny_cherno::Entity>());
+
     tiny_cherno::TinyChernoRuntime::GetRuntime()
         ->event_dispatcher.RegisterListener(
             tiny_cherno::EventType::KeyEvent, [](tiny_cherno::Event &e) {
-                printf("Received a key event! ");
                 class tiny_cherno::KeyEvent keyEvent =
                     static_cast<class tiny_cherno::KeyEvent &>(e);
-
-                printf("%i: %i\n", keyEvent.key, keyEvent.action);
                 return true;
             });
+
+    scene.Update();
     tiny_cherno::run();
 }
