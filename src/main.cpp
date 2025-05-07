ebada615580a11920faadce5005c9725b22f8c8c
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-07 14:22:38 +0200

    rgb square!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
-#include "cherry_pink.hpp"
-#include "component/mesh_system.hpp"
-#include "rendering/mesh.hpp"
+#include "component/transform_component.hpp"
+#include "rendering/material.hpp"
+#include <cherry_pink.hpp>
+#include <cmath>
+#include <component/mesh_system.hpp>
+#include <rendering/mesh.hpp>
+
 #include <event/key_event.hpp>
 #include <iostream>
 #include <memory>
 
-class RotateSystem : public cherrypink::System<cherrypink::TransformComponent> {
-    void ProcessComponent(const cherrypink::UUID &entityUuid, cherrypink::TransformComponent &transform) override {
+class RotateSystem : public cherrypink::System<cherrypink::Transform> {
+    void ProcessComponent(const cherrypink::UUID &entityUuid, cherrypink::Transform &transform) override {
         transform.rotation.y += 5.0f;
         transform.rotation.z += 2.0f;
+    }
+};
+
+class RainbowSystem : public cherrypink::System<cherrypink::ShaderMaterial> {
+    void ProcessComponent(const cherrypink::UUID &entityUuid, cherrypink::ShaderMaterial &material) override {
+        material.color.SetRed((std::sin(cherrypink::Ticks() / 20.0f) + 1.0) / 2.0f);
+        material.color.SetBlue((std::sin(cherrypink::Ticks() / 15.0f) + 1.0) / 2.0f);
+        material.color.SetGreen((std::sin(cherrypink::Ticks() / 10.0f) + 1.0) / 2.0f);
     }
 };
 
@@ -22,7 +34,8 @@ int main() {
 
     auto entity = cherrypink::CurrentScene().SpawnEntity();
 
-    cherrypink::Systems().RegisterSystem<cherrypink::TransformComponent>(std::make_shared<RotateSystem>());
+    cherrypink::Systems().RegisterSystem<cherrypink::Transform>(std::make_shared<RotateSystem>());
+    cherrypink::Systems().RegisterSystem<cherrypink::ShaderMaterial>(std::make_shared<RainbowSystem>());
 
     cherrypink::Events().RegisterListener<cherrypink::KeyEvent>([](const cherrypink::KeyEvent &keyEvent) {
                 std::cout << "Key " << keyEvent.key << " action: " << keyEvent.action << '\n';
@@ -37,8 +50,9 @@ int main() {
              0.5, -0.5, 0,
         }, {0, 1, 2, 3, 0, 2});
 
-    cherrypink::CurrentScene().componentRegistry.AttachComponent<cherrypink::TransformComponent>(entity.Uuid);
+    cherrypink::CurrentScene().componentRegistry.AttachComponent<cherrypink::Transform>(entity.Uuid);
     cherrypink::CurrentScene().componentRegistry.AttachComponent<cherrypink::MeshComponent>(entity.Uuid, cherrypink::MeshComponent { quad });
+    cherrypink::CurrentScene().componentRegistry.AttachComponent<cherrypink::ShaderMaterial>(entity.Uuid, cherrypink::ShaderMaterial { cherrypink::RGBAColor(1, 0, 1) });
 
     cherrypink::Run();
 }
